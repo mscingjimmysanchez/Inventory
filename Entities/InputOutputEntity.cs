24f9077876fc54d2fc0e4bbe179aa079e5a369d1
Jimmy Sánchez <39102908+mscingjimmysanchez@users.noreply.github.com>
2022-06-08 21:38:34 -0500

    Add files via upload

---
+++ Entities/InputOutputEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class InputOutputEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string InOutId { get; set; }
+
+        [Required]
+        public DateTime InOutDate { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public bool IsInput { get; set; }
+
+        public string StorageId { get; set; }
+
+        public StorageEntity Storage { get; set; }
+    }
+}
---
+++ Entities/StorageEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class StorageEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string StorageId { get; set; }
+
+        [Required]
+        public DateTime LastUpdate { get; set; }
+
+        [Required]
+        public int PartialQuantity { get; set; }
+
+        public string ProductId { get; set; }
+
+        public ProductEntity Product { get; set; }
+
+        public string WarehouseId { get; set; }
+
+        public WarehouseEntity Warehouse { get; set; }
+
+        public ICollection<InputOutputEntity> InputOutputs { get; set; }
+    }
+}
